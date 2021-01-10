21640e6569bec8a22ea6ac3ecae58e6ef05b51f1
b0101520154 <71687764+b0101520154@users.noreply.github.com>
2021-01-10 16:33:04 +0800

    發布測試

--- VR/Assets/Scripts/ScoreManager.cs
+++ VR/Assets/Scripts/ScoreManager.cs
@@ -1,6 +1,36 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
+    [Header("分數介面")]
+    public Text textScore;
+    [Header("分數")]
+    public int score;
+    [Header("打到分數")]
+    public int scoreIn = 2;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag =="神奇球球")
+        {
+            AddScore();
+        }
+        if (other.tag == "聯結球")
+        {
+            scoreIn = 3;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "聯結球")
+        {
+            scoreIn = 2;
+        }
+    }
+    private void AddScore()
+    {
+        score += scoreIn;
+        textScore.text = "目前得分:" + score;
+    }
 }
